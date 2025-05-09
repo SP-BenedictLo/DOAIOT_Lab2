13dc66ffb71887739ed8b46b6e5737ced942a922
SP-BenedictLo <BENEDICTLO.24@ichat.sp.edu.sg>
2025-05-09 12:24:37 +0800

    Modified bmi.py to be correct

--- bmi.py
+++ bmi.py
@@ -1,7 +1,7 @@
-def calculate_bmi(height, weight):
+def calculate_bmi(weight, height):
     print(f"Height = {height}")
     print(f"Weight = {weight}")
-    bmi = round(weight/height**2, 2)
+    bmi = round(weight/(height**2), 2)
     print(f"Your bmi is: {bmi}")
     if bmi < 18.5:
         print("You are underweight")
@@ -13,4 +13,4 @@ def calculate_bmi(height, weight):
         print("You are overweight")
         return 1
 
-calculate_bmi(weight= 57, height= 1.73)
+calculate_bmi(weight= 90, height= 1.73)
